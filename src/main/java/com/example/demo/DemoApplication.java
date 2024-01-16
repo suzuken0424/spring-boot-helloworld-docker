4c80ec111f86d119652ce3914b62285592b3b7d1
furyu-suzuki-kenta <suzuki.kenta@furyu.jp>
2024-01-16 11:33:18 +0900

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+	@Autowired
+	DataSource ds;
+
+	@RequestMapping("/")
+	String Index() {
+		JdbcTemplate template = new JdbcTemplate(ds);
+        return template.queryForObject("SELECT content FROM samples", String.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
